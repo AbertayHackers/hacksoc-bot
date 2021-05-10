0081b508ff39e4e9fe3c7927e3baebf5b64ce609
AG <github@muirlandoracle.co.uk>
2021-05-10 01:46:05 +0000

    Added misc cog w/ verify command

---
+++ cogs/misc.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+from libs.loadconf import config, formatHelp
+
+class Misc(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command(name="version", description=formatHelp("version", "desc"), usage=formatHelp("version", "usage"))
+	async def contributors(self, ctx):
+		await ctx.channel.send(config["version"])
+				
+
+
+def setup(bot):
+	bot.add_cog(Misc(bot))
