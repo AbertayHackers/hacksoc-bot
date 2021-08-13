9b6def5713cf8698d35949cbad18618b3a642b39
AG <github@muirlandoracle.co.uk>
2021-08-13 15:41:57 +0000

    Added Github command

--- cogs/contributors.py
+++ cogs/contributors.py
@@ -25,6 +25,14 @@ class Contributors(commands.Cog):
             embed.add_field(name=i, value=text, inline=False)
         await ctx.channel.send(embed=embed)
 
+    @commands.command(name="github", description=formatHelp("github", "desc"), usage=formatHelp("github", "usage"))
+    async def github(self, ctx):
+        embed = discord.Embed(colour=discord.Color.blue())
+        embed.set_thumbnail(url="https://github.com/fluidicon.png")
+        embed.title = "Contribute to the Bot:"
+        embed.description = strings["links"]["botGithub"]
+        await ctx.channel.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Contributors(bot))
