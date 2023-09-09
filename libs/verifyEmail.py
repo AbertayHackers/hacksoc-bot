22e63dbd124ffe6e9f33c2211ae3dc3091ca2d67
Waveyyyy <Waves-sec@protonmail.com>
2023-09-09 19:14:19 +0100

    Add data required for the required usage of the API
    URI used includes a 5 second (5000 millisecond) wait time for each
    request to complete, this seemed to be the sweet spot with most requests
    taking 2-3 seconds.
    Email to be verified is sent in the JSON format
    HTTPBasicAuth is used to authenticate as requesting a bearer token each
    time was deemed unnessecary

--- libs/verifyEmail.py
+++ libs/verifyEmail.py
@@ -2,5 +2,22 @@
 from libs.loadconf import secrets
 
 
-def verifaliaAPI():
-    pass
+def verifaliaAPI(email):
+    # the base url is https://api.verifalia.com/v2.4 as it removes the need
+    # for implementing an auto retry system
+
+    # rate limits are 6 https requests per second with a max burst of
+    # 15 https requests per second with unlimited concurrent email verification jobs
+    # if for whatever reason many emails need to be done at once use a single email job
+    # this rate limit is not likely to be hit by us
+    verifyURI = 'https://api.verifalia.com/v2.4/email-validations?waitTime=5000'
+    authentication = HTTPBasicAuth(
+        secrets['verifaliaOneID'], secrets['verifaliaOneKey'])
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        "entries": [
+            {
+                "inputData": f"{email}"
+            }
+        ]
+    }
