bdb9debcb28c9c7edeadd30fe61c337f5f21c656
Waveyyyy <Waves-sec@protonmail.com>
2023-09-10 17:32:40 +0100

    Add case for response code 202 from API requests
    This code indicates that the job was queued for completion. To handle
    this case multiple addittional requests need to be made.
    Firstly to be able to check job status we need the ID of the job, this
    is found under `["overview"]["id"]`. On subsequent requests the ID is
    appended to the `/email-validations/` endpoint.
    Each time the API responds with 202 on this new endpoint, it is telling
    us to check again later, i'm taking later as meaning 1 second. After a
    couple cycles a 200 response is returned and the job is complete, again
    checking the status and classification allows to determine the validity
    
    - to force the API to respond with 202 for testing, you can set the waitTime
      parameter to 0

--- libs/verifyEmail.py
+++ libs/verifyEmail.py
@@ -22,8 +22,32 @@ def verifaliaAPI(email):
         ]
     }
     response = requests.post(
-       verifyURI, auth = authentication, headers = headers, data = json.dumps(data))
-    if response.status_code == 200:
+        verifyURI, auth=authentication, headers=headers, data=json.dumps(data))
+    if response.status_code == 202:
+        # responds with 202 if request was queued (shouldn't happen often)
+        jobID = response.json()["overview"]["id"]
+        jobURI = f'https://api.verifalia.com/v2.4/email-validations/{jobID}'
+        # submitting a get request to this endpoint returns information about
+        # the job such as completion status and validation results for any entries
+        response = requests.get(jobURI, auth=authentication)
+        # if response is 202 keep checking every second until it isn't
+        while response.status_code == 202:
+            sleep(1)
+            response = requests.get(jobURI, auth=authentication)
+        # check if the job is completed
+        if response.status_code == 200 and response.json()["overview"]["status"] == "Completed":
+            entryData = response.json()["entries"]["data"][0]
+            # these two values are all thats required to determine validity of emails
+            if entryData["status"] != 'success' and entryData["classification"] != 'Deliverable':
+                print("Invalid Student ID")
+                print(entryData["status"])
+                print(entryData["classification"])
+            else:
+                print("Valid Student ID")
+                print(entryData["status"])
+                print(entryData["classification"])
+
+    elif response.status_code == 200:
         # will contain the verification result
         entryData = response.json()["entries"]["data"][0]
         print(response.status_code)
